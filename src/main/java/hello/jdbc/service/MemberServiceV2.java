138814508f1148f00a9957e49fcb13c804a5503f
SONGSUNGGEUN <abdf13423@naver.com>
2022-09-20 20:53:44 +0900

    spring-data-01

--- src/main/java/hello/jdbc/service/MemberServiceV2.java
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -19,7 +19,7 @@ public class MemberServiceV2 {
 
     private final DataSource dataSource;
     private final MemberRepositoryV2 memberRepository;
-    
+
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
         
         // 커넥션 생성
@@ -31,6 +31,7 @@ public class MemberServiceV2 {
 
             // 비즈니스 로직
             bizLogic(con, fromId, toId, money);
+
             con.commit(); // 성공 시 커밋
             
         } catch(Exception e) {
@@ -41,13 +42,16 @@ public class MemberServiceV2 {
         }
     }
 
+    // 비즈니스 로직
     private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+
         Member fromMember = memberRepository.findById(con, fromId);
         Member toMember = memberRepository.findById(con, toId);
 
         memberRepository.update(con, fromId, fromMember.getMoney() - money);
         validation(toMember);
         memberRepository.update(con, toId, toMember.getMoney() + money);
+        
     }
 
     private void release(Connection con) {
